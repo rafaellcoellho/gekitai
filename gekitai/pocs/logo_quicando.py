e7fbe84d0adacfa4d17352afa9243dbe2b5ddb95
Rafael Coelho <rafaellcoellho@gmail.com>
2023-02-27 21:42:05 -0300

    Revert "refatora lógica do comando em uma classe chamada Jogo"
    This reverts commit 10f95c3d53c5e7352f7c0621887e1a79791a6335.

--- gekitai/pocs/logo_quicando.py
+++ gekitai/pocs/logo_quicando.py
@@ -5,7 +5,6 @@ import pygame
 from pygame.surface import Surface
 from pygame.time import Clock
 
-
 TAMANHO_DA_JANELA: Tuple[int, int] = (500, 300)
 COR_DO_BACKGROUND: Tuple[int, int, int] = (100, 100, 100)
 
@@ -13,51 +12,32 @@ COR_DO_CIRCULO: Tuple[int, int, int] = (200, 200, 200)
 RAIO_DO_CIRCULO: int = 20
 
 
-class Jogo:
-    def __init__(self):
-        self.executando: bool = False
-        self.dx: int = 3
-        self.dy: int = 4
-        self.x: int = 100
-        self.y: int = 100
+def executar_logo_quicando():
+    dx: int = 3
+    dy: int = 4
+    x: int = 100
+    y: int = 100
 
-        self.relogio: Clock = pygame.time.Clock()
-        self.display: Surface = pygame.display.set_mode(
-            TAMANHO_DA_JANELA, pygame.SRCALPHA, 32
-        )
+    pygame.init()
+    clock: Clock = pygame.time.Clock()
+    display: Surface = pygame.display.set_mode(TAMANHO_DA_JANELA, pygame.SRCALPHA, 32)
 
-    def run(self):
-        pygame.init()
-        self.executando = True
+    while True:
+        clock.tick(30)
 
-        while self.executando:
-            self.processar_entrada_de_usuario()
-            self.atualizar_estado()
-            self.atualizar_graficos()
-            self.relogio.tick(30)
-
-    def processar_entrada_de_usuario(self):
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                self.executando = False
+                return
 
-    def atualizar_estado(self):
-        self.x = self.x + self.dx
-        self.y = self.y + self.dy
+        display.fill(COR_DO_BACKGROUND)
 
-        if self.x < RAIO_DO_CIRCULO or self.x > 500 - RAIO_DO_CIRCULO:
-            self.dx = -self.dx
-        if self.y < RAIO_DO_CIRCULO or self.y > 300 - RAIO_DO_CIRCULO:
-            self.dy = -self.dy
+        x = x + dx
+        y = y + dy
+        pygame.draw.circle(display, COR_DO_CIRCULO, (x, y), RAIO_DO_CIRCULO)
 
-    def atualizar_graficos(self):
-        self.display.fill(COR_DO_BACKGROUND)
-        pygame.draw.circle(
-            self.display, COR_DO_CIRCULO, (self.x, self.y), RAIO_DO_CIRCULO
-        )
+        if x < RAIO_DO_CIRCULO or x > 500 - RAIO_DO_CIRCULO:
+            dx = -dx
+        if y < RAIO_DO_CIRCULO or y > 300 - RAIO_DO_CIRCULO:
+            dy = -dy
+
         pygame.display.update()
-
-
-def executar_logo_quicando():
-    game = Jogo()
-    game.run()

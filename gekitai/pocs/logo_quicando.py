cdcc3f76b1fb8f7290544883c83d5f5e1b6e67aa
Rafael Coelho <rafaellcoellho@gmail.com>
2023-02-27 23:34:22 -0300

    implementa corretamente o modo de log quicando

--- gekitai/pocs/logo_quicando.py
+++ gekitai/pocs/logo_quicando.py
@@ -1,43 +1,45 @@
-from typing import Tuple
-
 import pygame
-
-from pygame.surface import Surface
-from pygame.time import Clock
-
-TAMANHO_DA_JANELA: Tuple[int, int] = (500, 300)
-COR_DO_BACKGROUND: Tuple[int, int, int] = (100, 100, 100)
-
-COR_DO_CIRCULO: Tuple[int, int, int] = (200, 200, 200)
-RAIO_DO_CIRCULO: int = 20
 
 
 def executar_logo_quicando():
-    dx: int = 3
-    dy: int = 4
-    x: int = 100
-    y: int = 100
-
     pygame.init()
-    clock: Clock = pygame.time.Clock()
-    display: Surface = pygame.display.set_mode(TAMANHO_DA_JANELA, pygame.SRCALPHA, 32)
+
+    largura_da_janela = 800
+    altura_da_janela = 600
+    janela = pygame.display.set_mode((largura_da_janela, altura_da_janela))
+
+    imagem_logo_gekitai = pygame.image.load("assets/gekitai_logo.png")
+    retangulo_da_imagem = imagem_logo_gekitai.get_rect()
+
+    pos_x_da_imagem = 0
+    pos_y_da_imagem = 0
+    velocidade_do_eixo_x_para_imagem = 2
+    velocidade_do_eixo_y_para_imagem = 2
+
+    relogio = pygame.time.Clock()
 
     while True:
-        clock.tick(30)
-
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
                 return
 
-        display.fill(COR_DO_BACKGROUND)
+        pos_x_da_imagem += velocidade_do_eixo_x_para_imagem
+        pos_y_da_imagem += velocidade_do_eixo_y_para_imagem
 
-        x = x + dx
-        y = y + dy
-        pygame.draw.circle(display, COR_DO_CIRCULO, (x, y), RAIO_DO_CIRCULO)
+        if (
+            pos_x_da_imagem < 0
+            or pos_x_da_imagem + retangulo_da_imagem.width > largura_da_janela
+        ):
+            velocidade_do_eixo_x_para_imagem *= -1
+        if (
+            pos_y_da_imagem < 0
+            or pos_y_da_imagem + retangulo_da_imagem.height > altura_da_janela
+        ):
+            velocidade_do_eixo_y_para_imagem *= -1
 
-        if x < RAIO_DO_CIRCULO or x > 500 - RAIO_DO_CIRCULO:
-            dx = -dx
-        if y < RAIO_DO_CIRCULO or y > 300 - RAIO_DO_CIRCULO:
-            dy = -dy
-
+        janela.fill((0, 0, 0))
+        janela.blit(imagem_logo_gekitai, (pos_x_da_imagem, pos_y_da_imagem))
         pygame.display.update()
+
+        relogio.tick(60)

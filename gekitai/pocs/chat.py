fdc66f4a334f8c67fc814dc54705e35ff147863c
Rafael Coelho <rafaellcoellho@gmail.com>
2023-03-01 14:52:37 -0300

    faz parse dos comandos da poc de chat no cli

--- gekitai/pocs/chat.py
+++ gekitai/pocs/chat.py
@@ -1,43 +1,6 @@
-import pygame
-import pygame_gui
+def modo_exemplo_chat_servidor(porta):
+    print(f"servidor escutando na porta {porta}")
 
 
-def modo_exemplo_chat():
-    pygame.init()
-
-    pygame.display.set_caption("Exemplo de chat")
-    janela = pygame.display.set_mode((800, 600))
-
-    fundo = pygame.Surface((800, 600))
-    fundo.fill(pygame.Color("#000000"))
-
-    gerenciador_de_interface = pygame_gui.UIManager((800, 600))
-
-    botao = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((350, 275), (100, 50)),
-        text="Foo",
-        manager=gerenciador_de_interface,
-    )
-
-    relogio = pygame.time.Clock()
-
-    while True:
-        variacao_de_tempo = relogio.tick(60) / 1000.0
-
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-            if evento.type == pygame_gui.UI_BUTTON_PRESSED:
-                if evento.ui_element == botao:
-                    print("Bar!")
-
-            gerenciador_de_interface.process_events(evento)
-
-        gerenciador_de_interface.update(variacao_de_tempo)
-
-        janela.blit(fundo, (0, 0))
-        gerenciador_de_interface.draw_ui(janela)
-
-        pygame.display.update()
+def modo_exemplo_chat_cliente(ip, porta):
+    print(f"conectando no servidor no endereço {ip} na porta {porta}")

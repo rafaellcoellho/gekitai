6885485b4f9ea018b9326e13ba4bbb9a183de576
Rafael Coelho <rafaellcoellho@gmail.com>
2023-03-06 19:03:48 -0300

    adiciona tratamento de mensagem para passar turno e desistir

--- gekitai/jogo/main.py
+++ gekitai/jogo/main.py
@@ -3,39 +3,49 @@ import pygame_gui
 import selectors
 import socket
 import threading
+import re
 
 
 def main(papel, ip, porta):
     # constantes
     tamanho_da_janela = (936, 655)
     cor_de_fundo = (255, 255, 255)
+
     identificacao_do_servidor_no_chat = "<font color=#46B8F7>servidor</font>"
     identificacao_do_cliente_no_chat = "<font color=#C65454>cliente</font>"
 
+    parser_do_comando_criar_peca = re.compile(r"^CNP=\((\d), (\d)\)$")
+    parser_do_comando_remover_peca = re.compile(r"^RPE=\((\d), (\d)\)$")
+    parser_do_comando_mensagem_do_chat = re.compile(r"^CHT=(.*)$")
+
     # estado inicial do jogo
-    turno_do_jogador = "azul"
-    estado_do_tabuleiro = [
-        ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
-        ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
-        ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
-        ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
-        ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
-        ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
-    ]
-    pecas = []
+    estado_do_jogo = {
+        "ganhador": "nenhum",
+        "executando": True,
+        "turno_do_jogador": "servidor",
+        "estado_do_tabuleiro": [
+            ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
+            ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
+            ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
+            ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
+            ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
+            ["vazio", "vazio", "vazio", "vazio", "vazio", "vazio"],
+        ],
+    }
+    interface_grafica_das_pecas_no_tabuleiro = []
 
     # carregando assets
     imagem_do_tabuleiro = pygame.image.load("assets/tabuleiro.png")
-    imagem_do_retrato_jogador_azul = pygame.transform.smoothscale(
+    imagem_do_retrato_jogador_servidor = pygame.transform.smoothscale(
         pygame.image.load("assets/retrato_jogador_azul.png"), (54, 69)
     )
-    imagem_do_retrato_jogador_vermelho = pygame.transform.smoothscale(
+    imagem_do_retrato_jogador_cliente = pygame.transform.smoothscale(
         pygame.image.load("assets/retrato_jogador_vermelho.png"), (54, 69)
     )
-    imagem_da_peca_jogador_azul = pygame.transform.smoothscale(
+    imagem_da_peca_jogador_servidor = pygame.transform.smoothscale(
         pygame.image.load("assets/peca_jogador_azul.png"), (80, 80)
     )
-    imagem_da_peca_jogador_vermelho = pygame.transform.smoothscale(
+    imagem_da_peca_jogador_cliente = pygame.transform.smoothscale(
         pygame.image.load("assets/peca_jogador_vermelho.png"), (80, 80)
     )
     imagem_borda_status_turno = pygame.transform.smoothscale(
@@ -77,7 +87,7 @@ def main(papel, ip, porta):
             (0, 10),
             (
                 imagem_do_tabuleiro.get_width(),
-                imagem_do_retrato_jogador_azul.get_height(),
+                imagem_do_retrato_jogador_servidor.get_height(),
             ),
         ),
         manager=gerenciador_de_interface_grafica,
@@ -93,30 +103,30 @@ def main(papel, ip, porta):
         manager=gerenciador_de_interface_grafica,
         container=interface_de_controle_dos_turnos,
     )
-    retrato_jogador_azul = pygame_gui.elements.UIImage(
+    retrato_jogador_servidor = pygame_gui.elements.UIImage(
         relative_rect=pygame.Rect(
             (0, 0),
             (
-                imagem_do_retrato_jogador_azul.get_width(),
-                imagem_do_retrato_jogador_azul.get_height(),
+                imagem_do_retrato_jogador_servidor.get_width(),
+                imagem_do_retrato_jogador_servidor.get_height(),
             ),
         ),
-        image_surface=imagem_do_retrato_jogador_azul,
+        image_surface=imagem_do_retrato_jogador_servidor,
         manager=gerenciador_de_interface_grafica,
         container=interface_de_status_do_turno,
     )
-    retrato_jogador_vermelho = pygame_gui.elements.UIImage(
+    retrato_jogador_cliente = pygame_gui.elements.UIImage(
         relative_rect=pygame.Rect(
             (10, 0),
             (
-                imagem_do_retrato_jogador_vermelho.get_width(),
-                imagem_do_retrato_jogador_vermelho.get_height(),
+                imagem_do_retrato_jogador_cliente.get_width(),
+                imagem_do_retrato_jogador_cliente.get_height(),
             ),
         ),
-        image_surface=imagem_do_retrato_jogador_vermelho,
+        image_surface=imagem_do_retrato_jogador_cliente,
         manager=gerenciador_de_interface_grafica,
         container=interface_de_status_do_turno,
-        anchors={"left": "left", "left_target": retrato_jogador_azul},
+        anchors={"left": "left", "left_target": retrato_jogador_servidor},
     )
 
     interface_de_botoes_passar_e_desistir = pygame_gui.core.UIContainer(
@@ -200,9 +210,24 @@ def main(papel, ip, porta):
 
             if dados_recebidos_pela_rede:
                 mensagem_recebida = dados_recebidos_pela_rede.decode("utf-8")
-                log_de_mensagens.append_html_text(
-                    f"{identificacao_do_cliente_no_chat}: {mensagem_recebida}<br>"
-                )
+                if mensagem_recebida == "DST":
+                    estado_do_jogo["ganhador"] = "servidor"
+                    estado_do_jogo["executando"] = False
+                elif mensagem_recebida == "PAS":
+                    estado_do_jogo["turno_do_jogador"] = "servidor"
+                elif parser_do_comando_criar_peca.search(mensagem_recebida):
+                    coluna = mensagem_recebida[4]
+                    linha = mensagem_recebida[7]
+                    print(f"Comando de criar peça em ({coluna}, {linha})")
+                elif parser_do_comando_remover_peca.search(mensagem_recebida):
+                    coluna = mensagem_recebida[4]
+                    linha = mensagem_recebida[7]
+                    print(f"Comando de remover pela em ({coluna}, {linha})")
+                elif parser_do_comando_mensagem_do_chat.search(mensagem_recebida):
+                    conteudo = mensagem_recebida[4:]
+                    log_de_mensagens.append_html_text(
+                        f"{identificacao_do_cliente_no_chat}: {conteudo}<br>"
+                    )
             else:
                 seletores.unregister(file_descriptor_socket_cliente)
                 file_descriptor_socket_cliente.close()
@@ -241,9 +266,28 @@ def main(papel, ip, porta):
                 mensagem_recebida_do_servidor = (
                     dados_recebidos_pela_rede_do_servidor.decode("utf-8")
                 )
-                log_de_mensagens.append_html_text(
-                    f"{identificacao_do_servidor_no_chat}: {mensagem_recebida_do_servidor}<br>"
-                )
+                if mensagem_recebida_do_servidor == "DST":
+                    estado_do_jogo["ganhador"] = "cliente"
+                    estado_do_jogo["executando"] = False
+                elif mensagem_recebida_do_servidor == "PAS":
+                    estado_do_jogo["turno_do_jogador"] = "cliente"
+                elif parser_do_comando_criar_peca.match(mensagem_recebida_do_servidor):
+                    coluna = mensagem_recebida_do_servidor[4]
+                    linha = mensagem_recebida_do_servidor[7]
+                    print(f"Comando de criar peça em ({coluna}, {linha})")
+                elif parser_do_comando_remover_peca.match(
+                    mensagem_recebida_do_servidor
+                ):
+                    coluna = mensagem_recebida_do_servidor[4]
+                    linha = mensagem_recebida_do_servidor[7]
+                    print(f"Comando de remover pela em ({coluna}, {linha})")
+                elif parser_do_comando_mensagem_do_chat.match(
+                    mensagem_recebida_do_servidor
+                ):
+                    conteudo = mensagem_recebida_do_servidor[4:]
+                    log_de_mensagens.append_html_text(
+                        f"{identificacao_do_servidor_no_chat}: {conteudo}<br>"
+                    )
             else:
                 seletores.unregister(file_descriptor_socket_cliente)
                 file_descriptor_socket_cliente.close()
@@ -254,6 +298,15 @@ def main(papel, ip, porta):
     else:
         print("papel na rede invalido")
         return
+
+    def envia_mensagem_para_jogador_oponente(mensagem):
+        if papel == "servidor":
+            sockets_conectados[0].sendall(str.encode(f"{mensagem}"))
+        elif papel == "cliente":
+            instancia_do_socket.send(str.encode(f"{mensagem}"))
+        else:
+            print("papel na rede invalido")
+            return
 
     # inicia thread escutando por mensagens da rede
     def loop_escutando_mensagens_dos_seletores():
@@ -276,16 +329,34 @@ def main(papel, ip, porta):
     # relogio do jogo
     relogio = pygame.time.Clock()
 
-    while True:
+    while estado_do_jogo["executando"]:
         delta_de_tempo = relogio.tick(60)
 
         # processar input de usuário
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                return
+                estado_do_jogo["executando"] = False
             if evento.type == pygame.USEREVENT:
                 if evento.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                    if evento.ui_element == botao_de_enviar:
+                    if evento.ui_element == botao_de_desistir:
+                        envia_mensagem_para_jogador_oponente(f"DST")
+                        estado_do_jogo["ganhador"] = (
+                            "servidor" if papel == "cliente" else "servidor"
+                        )
+                    elif evento.ui_element == botao_de_passar_turno:
+                        if (
+                            papel == "servidor"
+                            and estado_do_jogo["turno_do_jogador"] == "servidor"
+                        ):
+                            envia_mensagem_para_jogador_oponente(f"PAS")
+                            estado_do_jogo["turno_do_jogador"] = "cliente"
+                        elif (
+                            papel == "cliente"
+                            and estado_do_jogo["turno_do_jogador"] == "cliente"
+                        ):
+                            envia_mensagem_para_jogador_oponente(f"PAS")
+                            estado_do_jogo["turno_do_jogador"] = "servidor"
+                    elif evento.ui_element == botao_de_enviar:
                         mensagem_para_enviar = entrada_de_texto.get_text()
                         if mensagem_para_enviar:
                             entrada_de_texto.set_text("")
@@ -297,18 +368,9 @@ def main(papel, ip, porta):
                             log_de_mensagens.append_html_text(
                                 f"{identificacao_jogador_no_chat}: {mensagem_para_enviar}<br>"
                             )
-
-                            if papel == "servidor":
-                                sockets_conectados[0].sendall(
-                                    str.encode(f"{mensagem_para_enviar}")
-                                )
-                            elif papel == "cliente":
-                                instancia_do_socket.send(
-                                    str.encode(f"{mensagem_para_enviar}")
-                                )
-                            else:
-                                print("papel na rede invalido")
-                                return
+                            envia_mensagem_para_jogador_oponente(
+                                f"CHT={mensagem_para_enviar}"
+                            )
 
             gerenciador_de_interface_grafica.process_events(evento)
 
@@ -316,9 +378,9 @@ def main(papel, ip, porta):
         gerenciador_de_interface_grafica.update(delta_de_tempo / 1000.0)
 
         retrato_jogador_com_turno_ativo = (
-            retrato_jogador_azul
-            if turno_do_jogador == "azul"
-            else retrato_jogador_vermelho
+            retrato_jogador_servidor
+            if estado_do_jogo["turno_do_jogador"] == "servidor"
+            else retrato_jogador_cliente
         )
 
         # atualizar gráficos
@@ -334,3 +396,6 @@ def main(papel, ip, porta):
         )
 
         pygame.display.update()
+
+    print(f"Parabéns ao ganhador do jogo: {estado_do_jogo['ganhador']}")
+    pygame.quit()
